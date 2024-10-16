b99042533b0edd83d5fd2192cbd7c2ffa321263d
Rishit Srivastava <rishitsrivastava43@gmail.com>
2024-10-16 19:27:54 +0530

    done with the layout, will have to create separate component for these

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,17 +3,19 @@ import React from 'react'
 
 const Layout: React.FC = () => {
 
-    return <div className="flex flex-col h-screen justify-center items-center bg-black text-white">
+    return <div>
+
     {/* <!-- Header --> */}
-    <header className="text-center border-b border-white">
-      <h1 className="text-3xl">Pomodoro Timer</h1>
-    </header>
-  
+    <div className="border-b-2 p-3 bg-black">
+      <div className="text-2xl text-gray-100 pl-5">Pomodoro Timer</div>
+    </div>
+    
+  <div className="flex flex-col h-screen justify-center items-center bg-black text-white">
     {/* <!-- Timer Display and Buttons --> */}
     <div className="flex flex-col items-center mt-10">
       {/* <!-- Timer --> */}
       <div className="w-64 h-40 flex justify-center items-center border-2 border-white rounded-lg mb-8">
-        <h2 className="text-4xl">00:00</h2>
+        <h2 className="text-4xl">00:00</h2> {/* need to make a timer container and then add it here */}
       </div>
   
       {/* <!-- Buttons (Start, Break, Restart) --> */}
@@ -30,7 +32,7 @@ const Layout: React.FC = () => {
     </div>
   
     {/* <!-- Right-side Time Selection (30 mins, 60 mins, 90 mins) --> */}
-    <div className="absolute right-10 top-1/2 transform -translate-y-1/2">
+    <div className="absolute right-52 top-1/2 transform -translate-y-1/2">
       <div className="flex flex-col space-y-4">
         {/* <!-- 30 Mins Button --> */}
         <button className="w-24 h-12 border-2 border-white rounded-lg hover:bg-gray-700">30 mins</button>
@@ -41,6 +43,7 @@ const Layout: React.FC = () => {
         {/* <!-- 90 Mins Button --> */}
         <button className="w-24 h-12 border-2 border-white rounded-lg hover:bg-gray-700">90 mins</button>
       </div>
+    </div>
     </div>
   </div>
   
